54019323867060731a2794270ddef4f6c965e64d
at200 <at200@duke.edu>
2017-03-25 02:48:52 -0400

    Successfully implemented hello world in react

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  // res.send('hello world');
+  res.redirect('/dist/index.html');
 });
 
 module.exports = app;
