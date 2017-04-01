3c03a50daf328d69ca1e1780720d56686155fa46
at200 <at200@duke.edu>
2017-04-01 00:55:08 -0400

    Login page basic UI

--- client/main.js
+++ client/main.js
@@ -1,9 +1,67 @@
+// import RaisedButton from 'material-ui/RaisedButton';
+//
+// const React = require('react');
+//
+// class Main extends React.Component {
+//   render() {
+//     return <div>Hello World!</div>;
+//   }
+// }
+//
+// module.exports = Main;
+//
+
 const React = require('react');
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 class Main extends React.Component {
-  render() {
-    return <div>Hello World!</div>;
+render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Welcome! Please register"
+           />
+           <TextField
+             hintText="Enter your First Name"
+             floatingLabelText="First Name"
+             onChange = {(event,newValue) => this.setState({first_name:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Last Name"
+             floatingLabelText="Last Name"
+             onChange = {(event,newValue) => this.setState({last_name:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Email"
+             type="email"
+             floatingLabelText="Email"
+             onChange = {(event,newValue) => this.setState({email:newValue})}
+             />
+           <br/>
+           <TextField
+             type = "password"
+             hintText="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+          <RaisedButton label="Submit" primary={true} style={style}/>
+          </div>
+         </MuiThemeProvider>
+      </div>
+    );
   }
 }
+
+const style = {
+  margin: 15,
+};
 
 module.exports = Main;
