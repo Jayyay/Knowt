5dc891e2a62461931b7fec01a94ad3b77793054c
at200 <at200@duke.edu>
2017-04-07 14:11:43 -0400

    Added testing UI

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const { render } = require('react-dom');
-const Main = require('./main.js');
+const Main = require('./components/Main.js').default;
 
 class App extends React.Component {
   render() {
---
+++ client/components/navBar.js
@@ -0,0 +1,27 @@
+const React = require('react');
+const Blueprint = require('@blueprintjs/core');
+
+class navBar extends React.Component {
+
+  render() {
+    return (
+    <nav className="pt-navbar pt-dark pt-fixed-top">
+        <div className="pt-navbar-group pt-align-left">
+          <div className="pt-navbar-heading">Blueprint</div>
+          <input className="pt-input" placeholder="Search files..." type="text" />
+        </div>
+
+      <div className="pt-navbar-group pt-align-right">
+        <button className="pt-button pt-minimal pt-icon-home">Home</button>
+        <button className="pt-button pt-minimal pt-icon-document">Files</button>
+        <span className="pt-navbar-divider"></span>
+        <button className="pt-button pt-minimal pt-icon-user"></button>
+        <button className="pt-button pt-minimal pt-icon-notifications"></button>
+        <button className="pt-button pt-minimal pt-icon-cog"></button>
+      </div>
+    </nav>
+    );
+  }
+}
+
+module.exports = navBar;
