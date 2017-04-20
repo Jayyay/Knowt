eac59606fdc5a5ea0b8d4543bb9cec3445a2bec8
Jay Wang <zw48@duke.edu>
2017-04-20 01:08:01 -0400

    Net id login

--- models/users.js
+++ models/users.js
@@ -28,7 +28,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       defaultValue: ""
     },
     email: {
